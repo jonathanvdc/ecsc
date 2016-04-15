1797573d109c326a28087ef067f01cb3c2dc267e
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-15 17:22:29 +0200

    Created ecsc console project

---
+++ src/ecsc/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flame.Front;
+using Flame.Front.Cli;
+
+namespace ecsc
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			// ProjectHandlers.RegisterHandler(new EcsProjectHandler());
+			var compiler = new ConsoleCompiler("ecsc", "the direct EC# compiler", "https://github.com/jonathanvdc/ecsc/releases");
+			Environment.Exit(compiler.Compile(args));
+		}
+	}
+}
