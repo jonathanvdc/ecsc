156e5362577ec167d50a105251aef2de72937dd6
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-16 14:47:41 +0200

    Added ConvertImplicit/ConvertExplicit methods to GlobalScope

--- src/Flame.Ecs/GlobalScope.cs
+++ src/Flame.Ecs/GlobalScope.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Flame.Compiler;
 using Flame.Build;
+using Flame.Compiler.Expressions;
+using Pixie;
 
 namespace Flame.Ecs
 {
@@ -35,6 +37,72 @@ namespace Flame.Ecs
 		public GlobalScope WithBinder(QualifiedBinder NewBinder)
 		{
 			return new GlobalScope(NewBinder, ConversionRules, Log, TypeNamer);
+		}
+
+		private static MarkupNode[] HighlightEven(params string[] Text)
+		{
+			var results = new MarkupNode[Text.Length];
+			for (int i = 0; i < Text.Length; i++)
+			{
+				results[i] = new MarkupNode(
+					i % 2 == 0 ? NodeConstants.TextNodeType : NodeConstants.BrightNodeType,
+					Text[i]);
+			}
+			return results;
+		}
+
+		/// <summary>
+		/// Implicitly converts the given expression to the given type.
+		/// A diagnostic is issued if this is not a legal operation,
+		/// but the resulting expression is always of the given target type,
+		/// and is never null.
+		/// </summary>
+		public IExpression ConvertImplicit(IExpression From, IType To, SourceLocation Location)
+		{
+			var result = ConversionRules.TryConvertImplicit(From, To);
+			if (result != null)
+			{
+				return result;
+			}
+			else
+			{
+				result = ConversionRules.TryConvertExplicit(From, To);
+				Log.LogError(new LogEntry(
+					"no implicit conversion", 
+					HighlightEven(
+						"cannot implicitly convert type '", TypeNamer.Convert(From.Type), "' to '", TypeNamer.Convert(To), "'." + 
+						(result != null ? " An explicit conversion exists. (are you missing a cast?)" : "")),
+					Location));
+				
+				if (result == null)
+					return new UnknownExpression(To);
+				else
+					return result;
+			}
+		}
+
+		/// <summary>
+		/// Explicitly converts the given expression to the given type.
+		/// A diagnostic is issued if this is not a legal operation,
+		/// but the resulting expression is always of the given target type,
+		/// and is never null.
+		/// </summary>
+		public IExpression ConvertExplicit(IExpression From, IType To, SourceLocation Location)
+		{
+			var result = ConversionRules.TryConvertExplicit(From, To);
+			if (result != null)
+			{
+				return result;
+			}
+			else
+			{
+				Log.LogError(new LogEntry(
+					"no conversion", 
+					HighlightEven("cannot convert type '", TypeNamer.Convert(From.Type), "' to '", TypeNamer.Convert(To), "'."),
+					Location));
+				
+				return new UnknownExpression(To);
+			}
 		}
 	}
 }
