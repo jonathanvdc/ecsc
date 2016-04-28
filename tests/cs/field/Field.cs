ed07e5ad3c21f3cb6eda38650fa6d0c4b94de38c
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-29 00:48:56 +0200

    Tweaked a test

--- tests/cs/field/Field.cs
+++ tests/cs/field/Field.cs
@@ -10,7 +10,7 @@ public static class Program
     public static void Main()
     {
         var vec = default(Vector2);
-        vec.x = vec.y = 3;
+        vec.x = vec.y = 3.0;
         Console.WriteLine(vec.x);
         Console.WriteLine(vec.y);
     }
