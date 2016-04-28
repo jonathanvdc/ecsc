7fa14820c5ffe3427f20e790741bcae949845d08
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-28 20:30:30 +0200

    Created a field test case

---
+++ tests/cs/field/Field.cs
@@ -0,0 +1,17 @@
+using System;
+
+public struct Vector2
+{
+    public double x, y;
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        var vec = default(Vector2);
+        vec.x = vec.y = 3;
+        Console.WriteLine(vec.x);
+        Console.WriteLine(vec.y);
+    }
+}
