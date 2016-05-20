66553dbc0eb8e8b50ddeccfeaedca30bce586100
jonathanvdc <jonathan.vdc@outlook.com>
2016-05-20 21:18:09 +0200

    Updated constructor test case

--- tests/cs/constructor/Constructor.cs
+++ tests/cs/constructor/Constructor.cs
@@ -1,14 +1,55 @@
 
 public struct Vector2
 {
-    public Vector2(int x, int y) { }
+    public Vector2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x, y;
+}
+
+public class Base
+{
+    public Base()
+    {
+
+    }
+
+    public Base(int x)
+    {
+        this.x = x;
+    }
+
+    public int x;
+}
+
+public class Derived : Base
+{
+    public Derived(int x)
+    {
+
+    }
+
+    // public Derived(ref int x)
+    //     : base(x)
+    // {
+
+    // }
+
+    public Derived(double x)
+        : this((int)x)
+    {
+
+    }
 }
 
 public class Program
 {
-    public Program(ref int x) 
+    public Program(ref int x)
     {
-        return; 
+        return;
     }
 
     public static void Main()
