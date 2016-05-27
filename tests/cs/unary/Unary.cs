32eab2a442878aa1d745b65eb50f5f1b09ac92f9
jonathanvdc <jonathan.vdc@outlook.com>
2016-05-27 20:21:05 +0200

    Added a test case for unary ops

---
+++ tests/cs/unary/Unary.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class Program
+{
+    public static void Main(string[] Args)
+    {
+        int x = 3;
+        Console.WriteLine(-0.5);
+        Console.WriteLine(-x);
+        Console.WriteLine(~x);
+        Console.WriteLine(!true);
+        Console.WriteLine(!false);
+    }
+}
