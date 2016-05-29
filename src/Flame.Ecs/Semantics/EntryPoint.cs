ea1a25183f5ce7b6f390bbcc00c06eed2acddbc3
jonathanvdc <jonathan.vdc@outlook.com>
2016-05-29 20:59:58 +0200

    Moved InferEntryPoint to Flame.Ecs

---
+++ src/Flame.Ecs/Semantics/EntryPoint.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Flame.Compiler;
+using Pixie;
+
+namespace Flame.Ecs.Semantics
+{
+    public static class EntryPoint
+    {
+        /// <summary>
+        /// Infers an assembly's entry point, 
+        /// which is any function that matches the pattern
+        /// `static void main(string[] Args)`.
+        /// </summary>
+        /// <param name="Assembly"></param>
+        /// <returns></returns>
+        public static IMethod InferEntryPoint(IAssembly Assembly, ICompilerLog Log)
+        {
+            IMethod result = null;
+            foreach (var type in Assembly.CreateBinder().GetTypes())
+            {
+                foreach (var method in type.GetMethods())
+                {
+                    // Basically match anything that looks like `static void main(...)`
+                    var name = method.Name as SimpleName;
+                    if (name != null && name.Name.Equals("main", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (name.TypeParameterCount == 0 && method.IsStatic
+                            && method.ReturnType.Equals(PrimitiveTypes.Void)
+                            && !method.DeclaringType.GetRecursiveGenericParameters().Any())
+                        {
+                            if (result != null)
+                            {
+                                Log.LogError(new LogEntry(
+                                    "multiple entry points",
+                                    new MarkupNode[] 
+                                    { 
+                                        new MarkupNode(NodeConstants.TextNodeType, "this program has more than one entry point."),
+                                        result.GetSourceLocation().CreateRemarkDiagnosticsNode("other entry point")
+                                    },
+                                    method.GetSourceLocation()));
+                            }
+                            result = method;
+                        }
+                        else
+                        {
+                            WarningDescription warn;
+                            string message;
+
+                            if (name.TypeParameterCount > 0)
+                            {
+                                warn = EcsWarnings.GenericMainSignatureWarning;
+                                message = "cannot be an entry point, because it is generic.";
+                            }
+                            else if (method.DeclaringType.GetRecursiveGenericParameters().Any())
+                            {
+                                warn = EcsWarnings.GenericMainSignatureWarning;
+                                message = "cannot be an entry point, because its declaring type is generic.";
+                            }
+                            else if (!method.IsStatic)
+                            {
+                                warn = EcsWarnings.InvalidMainSignatureWarning;
+                                message = "is an instance method, and cannot be an entry point.";
+                            }
+                            else
+                            {
+                                warn = EcsWarnings.InvalidMainSignatureWarning;
+                                message = "has the wrong signature to be an entry point.";
+                            }
+
+                            if (warn.UseWarning(Log.Options))
+                            {
+                                Log.LogWarning(new LogEntry(
+                                    "invalid main signature",
+                                    NodeHelpers.HighlightEven(
+                                        "'", name.ToString(),
+                                        "' " + message + " ")
+                                    .Concat(new MarkupNode[] { warn.CauseNode }),
+                                    method.GetSourceLocation()));
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
+
--- src/ecsc/EcsProjectHandler.cs
+++ src/ecsc/EcsProjectHandler.cs
@@ -74,89 +74,14 @@ namespace ecsc
 				asm.AddNamespace(item);
 			}
 
-            asm.EntryPoint = InferEntryPoint(asm, Parameters.Log);
+            asm.EntryPoint = EntryPoint.InferEntryPoint(asm, Parameters.Log);
 
 			return asm;
 		}
 
-		/// <summary>
-		/// Infers an assembly's entry point, which is any function that matches the following pattern:
-		/// `static void main(string[] Args)`.
-		/// </summary>
-		/// <param name="Assembly"></param>
-		/// <returns></returns>
-        private static IMethod InferEntryPoint(IAssembly Assembly, ICompilerLog Log)
-		{
-            IMethod result = null;
-			foreach (var type in Assembly.CreateBinder().GetTypes())
-			{
-				foreach (var method in type.GetMethods())
-				{
-					// Basically match anything that looks like `static void main(...)`
-                    var name = method.Name as SimpleName;
-                    if (name != null && name.Name.Equals("main", StringComparison.OrdinalIgnoreCase))
-                    {
-                        if (name.TypeParameterCount == 0 && method.IsStatic
-                            && method.ReturnType.Equals(PrimitiveTypes.Void)
-                            && !method.DeclaringType.GetRecursiveGenericParameters().Any())
-                        {
-                            if (result != null)
-                            {
-                                Log.LogError(new LogEntry(
-                                    "multiple entry points",
-                                    new MarkupNode[] 
-                                    { 
-                                        new MarkupNode(NodeConstants.TextNodeType, "this program has more than one entry point."),
-                                        result.GetSourceLocation().CreateRemarkDiagnosticsNode("other entry point")
-                                    },
-                                    method.GetSourceLocation()));
-                            }
-                            result = method;
-                        }
-                        else
-                        {
-                            WarningDescription warn;
-                            string message;
-
-                            if (name.TypeParameterCount > 0)
-                            {
-                                warn = EcsWarnings.GenericMainSignatureWarning;
-                                message = "cannot be an entry point, because it is generic.";
-                            }
-                            else if (method.DeclaringType.GetRecursiveGenericParameters().Any())
-                            {
-                                warn = EcsWarnings.GenericMainSignatureWarning;
-                                message = "cannot be an entry point, because its declaring type is generic.";
-                            }
-                            else if (!method.IsStatic)
-                            {
-                                warn = EcsWarnings.InvalidMainSignatureWarning;
-                                message = "is an instance method, and cannot be an entry point.";
-                            }
-                            else
-                            {
-                                warn = EcsWarnings.InvalidMainSignatureWarning;
-                                message = "has the wrong signature to be an entry point.";
-                            }
-
-                            if (warn.UseWarning(Log.Options))
-                            {
-                                Log.LogWarning(new LogEntry(
-                                    "invalid main signature",
-                                    NodeHelpers.HighlightEven(
-                                        "'", name.ToString(),
-                                        "' " + message + " ")
-                                        .Concat(new MarkupNode[] { warn.CauseNode }),
-                                    method.GetSourceLocation()));
-                            }
-                        }
-                    }
-				}
-			}
-			return result;
-		}
-
-		public static Task<INamespaceBranch[]> ParseCompilationUnitsAsync(List<IProjectSourceItem> SourceItems, CompilationParameters Parameters, IBinder Binder, IAssembly DeclaringAssembly)
+		public static Task<INamespaceBranch[]> ParseCompilationUnitsAsync(
+            List<IProjectSourceItem> SourceItems, CompilationParameters Parameters, 
+            IBinder Binder, IAssembly DeclaringAssembly)
 		{
 			var sink = new CompilerLogMessageSink(Parameters.Log);
 			var processor = new MacroProcessor(typeof(LeMP.Prelude.BuiltinMacros), sink);
@@ -166,7 +91,10 @@ namespace ecsc
 			return ParseCompilationUnitsAsync(SourceItems, Parameters, Binder, DeclaringAssembly, processor, sink);
 		}
 
-		public static Task<INamespaceBranch[]> ParseCompilationUnitsAsync(List<IProjectSourceItem> SourceItems, CompilationParameters Parameters, IBinder Binder, IAssembly DeclaringAssembly, MacroProcessor Processor, IMessageSink Sink)
+		public static Task<INamespaceBranch[]> ParseCompilationUnitsAsync(
+            List<IProjectSourceItem> SourceItems, CompilationParameters Parameters, 
+            IBinder Binder, IAssembly DeclaringAssembly, MacroProcessor Processor, 
+            IMessageSink Sink)
 		{
 			var units = new Task<INamespaceBranch>[SourceItems.Count];
 			for (int i = 0; i < units.Length; i++)
@@ -223,7 +151,9 @@ namespace ecsc
 			}
 		}
 
-		public static Task<INamespaceBranch> ParseCompilationUnitAsync(IProjectSourceItem SourceItem, CompilationParameters Parameters, IBinder Binder, IAssembly DeclaringAssembly, MacroProcessor Processor, IMessageSink Sink)
+		public static Task<INamespaceBranch> ParseCompilationUnitAsync(
+            IProjectSourceItem SourceItem, CompilationParameters Parameters, IBinder Binder, 
+            IAssembly DeclaringAssembly, MacroProcessor Processor, IMessageSink Sink)
 		{
 			Parameters.Log.LogEvent(new LogEntry("Status", "Parsing " + SourceItem.SourceIdentifier));
 			return Task.Run(() =>
@@ -251,7 +181,9 @@ namespace ecsc
 			});
 		}
 
-		public static IEnumerable<LNode> ParseNodes(string Text, string Identifier, IParsingService Service, MacroProcessor Processor, IMessageSink Sink)
+		public static IEnumerable<LNode> ParseNodes(
+            string Text, string Identifier, IParsingService Service, 
+            MacroProcessor Processor, IMessageSink Sink)
 		{
 			var lexer = Service.Tokenize(new UString(Text), Identifier, Sink);
 
@@ -260,14 +192,20 @@ namespace ecsc
 			return Processor.ProcessSynchronously(new VList<LNode>(nodes));
 		}
 
-		public static INamespaceBranch ParseCompilationUnit(IEnumerable<LNode> Nodes, GlobalScope Scope, IAssembly DeclaringAssembly)
+		public static INamespaceBranch ParseCompilationUnit(
+            IEnumerable<LNode> Nodes, GlobalScope Scope, IAssembly DeclaringAssembly)
 		{
 			return converter.Value.ConvertCompilationUnit(Scope, DeclaringAssembly, Nodes);
 		}
 
 		public IEnumerable<ParsedProject> Partition(IEnumerable<ParsedProject> Projects)
 		{
-			return new ParsedProject[] { new ParsedProject(Projects.First().CurrentPath, UnionProject.CreateUnion(Projects.Select(item => item.Project).ToArray())) };
+			return new ParsedProject[] 
+            { 
+                new ParsedProject(
+                    Projects.First().CurrentPath, 
+                    UnionProject.CreateUnion(Projects.Select(item => item.Project).ToArray())) 
+            };
 		}
 
 		public PassPreferences GetPassPreferences(ICompilerLog Log)
