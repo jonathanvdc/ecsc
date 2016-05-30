ea427811f23486d48446d8d1923f7ab2af1540f7
jonathanvdc <jonathan.vdc@outlook.com>
2016-05-30 19:17:13 +0200

    Created a test case

---
+++ tests/cs/properties/Properties.cs
@@ -0,0 +1,51 @@
+using System;
+
+public struct Vector2
+{
+    public Vector2(double X, double Y)
+    {
+        this.x = X;
+        this.Y = Y;
+    }
+
+    private double x;
+    public double X { get { return x; } private set { x = value; } }
+    public double Y { get; private set; }
+
+    public double this[int i]
+    {
+        get { return i == 1 ? Y : X; }
+        private set
+        {
+            if (i == 1)
+                Y = value;
+            else
+                X = value;
+        }
+    }
+
+    public double this[long i] => i == 1 ? Y : X;
+
+    public double LengthSquared
+    {
+        get { return X * X + Y * Y; }
+    }
+
+    public double Length => Math.Sqrt(LengthSquared);
+}
+
+public static class Program
+{
+    public static void Main(string[] Args)
+    {
+        var vec = new Vector2(3, 4);
+        Console.WriteLine(vec.X);
+        Console.WriteLine(vec.Y);
+        // Console.WriteLine(vec[0]);
+        // Console.WriteLine(vec[1]);
+        // Console.WriteLine(vec[0L]);
+        // Console.WriteLine(vec[1L]);
+        Console.WriteLine(vec.LengthSquared);
+        Console.WriteLine(vec.Length);
+    }
+}
