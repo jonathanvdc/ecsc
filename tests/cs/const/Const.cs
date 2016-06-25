e8387dfbcd2c766a498d3a97c209f51c7ea72745
jonathanvdc <jonathan.vdc@outlook.com>
2016-06-25 18:39:29 +0200

    Created a 'const' test

---
+++ tests/cs/const/Const.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+    public const double x = 6000;
+    public const int y = 3000;
+    public const bool z = true;
+
+    public static void Main()
+    {
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(z);
+    }
+}
