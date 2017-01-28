be15aae118adeea503461f8673a1f7a27f845193
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-28 13:24:14 +0100

    Add a bunch of new tests

--- tests/cs/params-methods/ParamsMethods.cs
+++ tests/cs/params-methods/ParamsMethods.cs
@@ -1,105 +1,62 @@
-// Based on the example at
-// https://msdn.microsoft.com/en-us/library/828t9b9h(v=vs.110).aspx
-
-// This code example demonstrates the Console.WriteLine() method.
-// Formatting for this example uses the "en-US" culture.
+// Based on the example from
+// https://msdn.microsoft.com/en-us/library/w5zay9db.aspx
 
 using System;
 
-enum Color { Yellow = 1, Blue, Green }
-
-class Sample
+public class MyClass
 {
-
-    static DateTime thisDate = DateTime.Now;
-
-    public static void Main()
+    public static void UseParams(params int[] list)
     {
-    Console.Clear();
+        for (int i = 0; i < list.Length; i++)
+        {
+            Console.Write(list[i] + " ");
+        }
+        Console.WriteLine();
+    }
 
-    // Format a negative integer or floating-point number in various ways.
-    Console.WriteLine("Standard Numeric Format Specifiers");
-    Console.WriteLine(
-        "(C) Currency: . . . . . . . . {0:C}\n" +
-        "(D) Decimal:. . . . . . . . . {0:D}\n" +
-        "(E) Scientific: . . . . . . . {1:E}\n" +
-        "(F) Fixed point:. . . . . . . {1:F}\n" +
-        "(G) General:. . . . . . . . . {0:G}\n" +
-        "    (default):. . . . . . . . {0} (default = 'G')\n" +
-        "(N) Number: . . . . . . . . . {0:N}\n" +
-        "(P) Percent:. . . . . . . . . {1:P}\n" +
-        "(R) Round-trip: . . . . . . . {1:R}\n" +
-        "(X) Hexadecimal:. . . . . . . {0:X}\n",
-        -123, -123.45f);
+    public static void UseParams2(params object[] list)
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            Console.Write(list[i] + " ");
+        }
+        Console.WriteLine();
+    }
 
-        // Format the current date in various ways.
-    Console.WriteLine("Standard DateTime Format Specifiers");
-    Console.WriteLine(
-        "(d) Short date: . . . . . . . {0:d}\n" +
-        "(D) Long date:. . . . . . . . {0:D}\n" +
-        "(t) Short time: . . . . . . . {0:t}\n" +
-        "(T) Long time:. . . . . . . . {0:T}\n" +
-        "(f) Full date/short time: . . {0:f}\n" +
-        "(F) Full date/long time:. . . {0:F}\n" +
-        "(g) General date/short time:. {0:g}\n" +
-        "(G) General date/long time: . {0:G}\n" +
-        "    (default):. . . . . . . . {0} (default = 'G')\n" +
-        "(M) Month:. . . . . . . . . . {0:M}\n" +
-        "(R) RFC1123:. . . . . . . . . {0:R}\n" +
-        "(s) Sortable: . . . . . . . . {0:s}\n" +
-        "(u) Universal sortable: . . . {0:u} (invariant)\n" +
-        "(U) Universal full date/time: {0:U}\n" +
-        "(Y) Year: . . . . . . . . . . {0:Y}\n",
-        thisDate);
+    static void Main()
+    {
+        // You can send a comma-separated list of arguments of the
+        // specified type.
+        UseParams(1, 2, 3, 4);
+        UseParams2(1, 'a', "test");
 
-// Format a Color enumeration value in various ways.
-    Console.WriteLine("Standard Enumeration Format Specifiers");
-    Console.WriteLine(
-        "(G) General:. . . . . . . . . {0:G}\n" +
-        "    (default):. . . . . . . . {0} (default = 'G')\n" +
-        "(F) Flags:. . . . . . . . . . {0:F} (flags or integer)\n" +
-        "(D) Decimal number: . . . . . {0:D}\n" +
-        "(X) Hexadecimal:. . . . . . . {0:X}\n",
-        Color.Green);
+        // A params parameter accepts zero or more arguments.
+        // The following calling statement displays only a blank line.
+        UseParams2();
+
+        // An array argument can be passed, as long as the array
+        // type matches the parameter type of the method being called.
+        int[] myIntArray = { 5, 6, 7, 8, 9 };
+        UseParams(myIntArray);
+
+        object[] myObjArray = { 2, 'b', "test", "again" };
+        UseParams2(myObjArray);
+
+        // The following call causes a compiler error because the object
+        // array cannot be converted into an integer array.
+        //UseParams(myObjArray);
+
+        // The following call does not cause an error, but the entire
+        // integer array becomes the first element of the params array.
+        UseParams2(myIntArray);
     }
 }
 /*
-This code example produces the following results:
+Output:
+    1 2 3 4
+    1 a test
 
-Standard Numeric Format Specifiers
-(C) Currency: . . . . . . . . ($123.00)
-(D) Decimal:. . . . . . . . . -123
-(E) Scientific: . . . . . . . -1.234500E+002
-(F) Fixed point:. . . . . . . -123.45
-(G) General:. . . . . . . . . -123
-    (default):. . . . . . . . -123 (default = 'G')
-(N) Number: . . . . . . . . . -123.00
-(P) Percent:. . . . . . . . . -12,345.00 %
-(R) Round-trip: . . . . . . . -123.45
-(X) Hexadecimal:. . . . . . . FFFFFF85
-
-Standard DateTime Format Specifiers
-(d) Short date: . . . . . . . 6/26/2004
-(D) Long date:. . . . . . . . Saturday, June 26, 2004
-(t) Short time: . . . . . . . 8:11 PM
-(T) Long time:. . . . . . . . 8:11:04 PM
-(f) Full date/short time: . . Saturday, June 26, 2004 8:11 PM
-(F) Full date/long time:. . . Saturday, June 26, 2004 8:11:04 PM
-(g) General date/short time:. 6/26/2004 8:11 PM
-(G) General date/long time: . 6/26/2004 8:11:04 PM
-    (default):. . . . . . . . 6/26/2004 8:11:04 PM (default = 'G')
-(M) Month:. . . . . . . . . . June 26
-(R) RFC1123:. . . . . . . . . Sat, 26 Jun 2004 20:11:04 GMT
-(s) Sortable: . . . . . . . . 2004-06-26T20:11:04
-(u) Universal sortable: . . . 2004-06-26 20:11:04Z (invariant)
-(U) Universal full date/time: Sunday, June 27, 2004 3:11:04 AM
-(Y) Year: . . . . . . . . . . June, 2004
-
-Standard Enumeration Format Specifiers
-(G) General:. . . . . . . . . Green
-    (default):. . . . . . . . Green (default = 'G')
-(F) Flags:. . . . . . . . . . Green (flags or integer)
-(D) Decimal number: . . . . . 3
-(X) Hexadecimal:. . . . . . . 00000003
-
+    5 6 7 8 9
+    2 b test again
+    System.Int32[]
 */
