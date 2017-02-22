f583f18e7590f2deb17370d6fbab82515a3364f0
jonathanvdc <jonathan.vdc@outlook.com>
2017-02-22 18:52:11 +0100

    Add a test for XML docs

---
+++ tests/cs/xml-docs/XmlDocs.cs
@@ -0,0 +1,17 @@
+using static System.Console;
+
+/// <summary>
+/// The Program class, which defines the application's entry point.
+/// </summary>
+public static class Program
+{
+    /** <summary>
+     * The application's entry point.
+     * </summary>
+     * <param name="Args">The application's argument list.</param>
+     * <returns>Nothing.</returns>*/
+    public static void Main(string[] Args)
+    {
+        WriteLine("Hello, XML docs!");
+    }
+}
