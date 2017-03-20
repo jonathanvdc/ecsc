1c5b54b49c53b25cb7845398744d117c590b6a64
jonathanvdc <jonathan.vdc@outlook.com>
2017-03-20 20:27:52 +0100

    Revert "Add a test to verify that -Whidden-null-check is suppressed by #builtin_warning_disable"
    This reverts commit 0a4a300d9a3100e76d9772bbe23842a3c1590a82.

--- tests/cs/foreach/Foreach.cs
+++ tests/cs/foreach/Foreach.cs
@@ -42,9 +42,6 @@ public static class Program
         foreach (var item in new List<string>(Args))
             Console.WriteLine(item);
 
-        foreach (var item in (IEnumerable<string>)new List<string>(Args))
-            Console.WriteLine(item);
-
         foreach (var item in StaticEnumerable)
             Console.WriteLine(item);
     }
