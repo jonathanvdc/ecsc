bc94795e8aa28cc2c6db146a82c58ee78853fc0e
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-27 18:03:55 +0200

    Optimize MemberCacheBase slightly

--- src/Flame.Ecs/MemberCacheBase.cs
+++ src/Flame.Ecs/MemberCacheBase.cs
@@ -12,9 +12,23 @@ namespace Flame.Ecs
     {
         public MemberCacheBase()
         {
+            this.directMemberCache = new Dictionary<IType, Dictionary<string, List<ITypeMember>>>();
             this.namedMemberCache = new Dictionary<Tuple<IType, string>, ITypeMember[]>();
         }
 
+        // This member cache consists of two dictionaries:
+        //
+        //     1. `directMemberCache` which maps types to a dictionary of member names to
+        //        lists of members defined directly by the types.
+        //
+        //     2. `namedMemberCache` which maps (type, member name) pairs to arrays of
+        //        non-hidden members defined either directly or indirectly by `type`.
+        //
+        // When type member lookup is performed, all members are first stored by name in
+        // the `directMemberCache`. Lookups for specific names then extract lists from
+        // `directMemberCache`. The results of those lookups are stored in `namedMemberCache`,
+        // so no element is ever extracted more than once from `directMemberCache`.
+        private Dictionary<IType, Dictionary<string, List<ITypeMember>>> directMemberCache;
         private Dictionary<Tuple<IType, string>, ITypeMember[]> namedMemberCache;
 
         /// <summary>
@@ -98,13 +112,9 @@ namespace Flame.Ecs
         {
             var ty = Key.Item1;
             var name = Key.Item2;
-            var results = new HashSet<ITypeMember>();
-            // First, look in the given type.
-            results.UnionWith(GetMembers(ty).Where(m =>
-            {
-                var sName = m.Name as SimpleName;
-                return sName != null && sName.Name == name;
-            }));
+
+            // First, look for members in `ty`.
+            var results = new HashSet<ITypeMember>(ExtractDirectMembers(ty, name));
 
             if (HidesAll(results))
                 return results.ToArray();
@@ -115,7 +125,7 @@ namespace Flame.Ecs
             {
                 results.UnionWith(
                     GetVisibleMembers(
-                        bType, name, 
+                        bType, name,
                         results.OfType<IMethod>().ToArray()));
 
                 if (results.Count > 0)
@@ -133,8 +143,67 @@ namespace Flame.Ecs
             return results.ToArray();
         }
 
+        /// <summary>
+        /// Extracts the list of type members with the given name from the
+        /// given type member cache. If no such list exists, then the empty
+        /// sequence is returned.
+        /// </summary>
+        /// <param name="Cache">The cache of type members for a given type.</param>
+        /// <param name="Name">The name of the type members to fetch.</param>
+        /// <returns>The list of type members with the given name.</returns>
+        private IEnumerable<ITypeMember> ExtractDirectMembersFromCache(
+            Dictionary<string, List<ITypeMember>> Cache, string Name)
+        {
+            List<ITypeMember> result;
+            if (Cache.TryGetValue(Name, out result))
+            {
+                // Remove the list from the cache so that the garbage
+                // collector may reclaim it sooner rather than later.
+                Cache.Remove(Name);
+                return result;
+            }
+            else
+            {
+                return Enumerable.Empty<ITypeMember>();
+            }
+        }
+
+        /// <summary>
+        /// Computes the list of type members with the given name defined
+        /// directly by the given type on the first invocation. Successive
+        /// invocations with the same arguments return empty lists.
+        /// </summary>
+        /// <param name="Type">The type whose direct members are searched for.</param>
+        /// <param name="Name">The name of the members to look for.</param>
+        /// <returns>A list of type members.</returns>
+        private IEnumerable<ITypeMember> ExtractDirectMembers(IType Type, string Name)
+        {
+            Dictionary<string, List<ITypeMember>> typeMemberDictionary;
+            if (!directMemberCache.TryGetValue(Type, out typeMemberDictionary))
+            {
+                typeMemberDictionary = new Dictionary<string, List<ITypeMember>>();
+                foreach (var member in GetMembers(Type))
+                {
+                    var sName = member.Name as SimpleName;
+                    if (sName != null)
+                    {
+                        List<ITypeMember> memberList;
+                        if (!typeMemberDictionary.TryGetValue(sName.Name, out memberList))
+                        {
+                            memberList = new List<ITypeMember>();
+                            typeMemberDictionary[sName.Name] = memberList;
+                        }
+                        memberList.Add(member);
+                    }
+                }
+                directMemberCache[Type] = typeMemberDictionary;
+            }
+            return ExtractDirectMembersFromCache(typeMemberDictionary, Name);
+        }
+
         public virtual void Dispose()
         {
+            directMemberCache = null;
             namedMemberCache = null;
         }
     }
