1523195157c7e25cd6489292c59cb60439c8b4cc
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-07 16:02:59 +0200

    Extend the Cast test case to include enum-to-enum casts

--- tests/cs/cast/Cast.cs
+++ tests/cs/cast/Cast.cs
@@ -12,10 +12,27 @@ public class B : A
     { }
 }
 
+public enum ByteEnum : byte
+{
+    Zero,
+    One,
+    Two,
+    Three
+}
+
+public enum LongEnum : long
+{
+    Zero,
+    One,
+    Two,
+    Three
+}
+
 public static class Program
 {
     public static void Main()
     {
+        // Test is/as
         B x = default(B);
         Console.WriteLine(x is A);
         Console.WriteLine(x is B);
@@ -23,5 +40,16 @@ public static class Program
         Console.WriteLine(x as B);
         Console.WriteLine((A)x);
         Console.WriteLine((B)x);
+
+        // Test that enum-to-enum conversions work.
+        Console.WriteLine((int)(LongEnum)ByteEnum.Zero);
+        Console.WriteLine((int)(LongEnum)ByteEnum.One);
+        Console.WriteLine((int)(LongEnum)ByteEnum.Two);
+        Console.WriteLine((int)(LongEnum)ByteEnum.Three);
+
+        Console.WriteLine((int)(ByteEnum)LongEnum.Zero);
+        Console.WriteLine((int)(ByteEnum)LongEnum.One);
+        Console.WriteLine((int)(ByteEnum)LongEnum.Two);
+        Console.WriteLine((int)(ByteEnum)LongEnum.Three);
     }
 }
