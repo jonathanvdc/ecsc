ad884f111c4cf9e1f0684d0ae04eddbb0412c7e1
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-15 17:51:44 +0200

    Add an indexing test

---
+++ tests/cs/index/Index.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        int[] arr = new int[1];
+        arr[0] = 1;
+        Console.WriteLine(arr[0]);
+    }
+}
