25ad63d7fa6a892616aff6622c2f297fb8362a7a
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-16 12:18:30 +0200

    Extend the indexing test

--- tests/cs/index/Index.cs
+++ tests/cs/index/Index.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 public static class Program
 {
@@ -7,5 +8,9 @@ public static class Program
         int[] arr = new int[1];
         arr[0] = 1;
         Console.WriteLine(arr[0]);
+
+        var list = new List<int>() { 0 };
+        list[0] = 1;
+        Console.WriteLine(list[0]);
     }
 }
