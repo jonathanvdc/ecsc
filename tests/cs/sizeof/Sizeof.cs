52cecb92a157794dc3ace6dc9d88d70759843b40
jonathanvdc <jonathan.vdc@outlook.com>
2017-06-14 18:54:39 +0200

    Create a sizeof test case

---
+++ tests/cs/sizeof/Sizeof.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(sizeof(byte));
+        Console.WriteLine(sizeof(int));
+        Console.WriteLine(sizeof(uint));
+        Console.WriteLine(sizeof(long));
+        Console.WriteLine(sizeof(float));
+        Console.WriteLine(sizeof(double));
+    }
+}
